4a9209f9ea5208bc06dde1b50e915bc9e8058b0a
Rick Hawkins <rick.hawkins@slalom.com>
2018-10-28 13:05:10 -0700

    Add Example Module

---
+++ web/modules/custom/example_module/js/copy-clipboard.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Copy using clipboard.js library.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.copyClipboard = {
+    attach: function (context) {
+      // Some great JavaScript will go here.
+    }
+  };
+
+}(jQuery, Drupal));
