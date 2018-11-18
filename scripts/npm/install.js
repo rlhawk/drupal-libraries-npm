2462fa25d2bd95dd1dca9d8df040d560975d7e1b
Rick Hawkins <rick.hawkins@slalom.com>
2018-11-18 13:04:12 -0800

    Overhaul install script

--- scripts/npm/install.js
+++ scripts/npm/install.js
@@ -18,21 +18,53 @@ config.paths = loadConfig('paths');
 // Load libraries configuration.
 config.libraries = loadConfig('libraries');
 
-// Copy the library files.
-Object.keys(config.libraries).forEach(function (key, index) {
-  copy_libraries(config.libraries[key]);
-});
+// Clean the libraries.
+for (var index in config.libraries) {
+  if (config.libraries.hasOwnProperty(index)) {
+    cleanLibrary(config.libraries[index]);
+  }
+}
+
+// Copy the libraries.
+for (var index in config.libraries) {
+  if (config.libraries.hasOwnProperty(index)) {
+    copyLibrary(config.libraries[index]);
+  }
+}
 
 // FUNCTIONS //
 
 function loadConfig(name) {
   let ymlFile = fs.readFileSync(config_path + '/' + name + '.yml', 'utf8');
-  let config = yaml.load(ymlFile);
+  let loadedConfig = yaml.load(ymlFile);
 
-  return config;
+  // If this is not the 'paths' configuration, replace any path tokens.
+  if (name != 'paths') {
+    loadedConfig = replacePathTokens(loadedConfig);
+  }
+
+  return loadedConfig;
 }
 
-function copy_libraries(element) {
+function cleanLibrary(element) {
+  let files = [];
+
+  // Ensure src is an array.
+  if (typeof element.src == 'string') {
+    element.src = [element.src];
+  }
+
+  // Determine the files to be deleted.
+  element.src.forEach(function (filePath) {
+    var fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+    files.push(element.dest + '/' + fileName);
+  })
+
+  // Delete the files.
+  del(files);
+}
+
+function copyLibrary(element) {
   let paths = [];
 
   // The 'flat' option doesn't flatten the file structure, which must be a
@@ -56,9 +88,33 @@ function copy_libraries(element) {
   paths.push(...element.src);
 
   // Add the destination path.
-  element.dest = element.dest.replace("{libraries}", config.paths.libraries);
   paths.push(element.dest);
 
   // Copy the files.
   return copy(paths, options, function () { });
+}
+
+function replacePathTokens(e) {
+  var search = /\{([^}]*)\}/gi;
+
+  if (Object.prototype.toString.call(e) === '[object Array]') {
+    e.forEach(function (val, index) {
+      e[index] = replacePathTokens(val)
+    })
+  } else if ((typeof e === 'object') && (e !== null)) {
+    for (var index in e) {
+      if (e.hasOwnProperty(index)) {
+        e[index] = replacePathTokens(e[index]);
+      }
+    }
+  } else {
+    // Replace any path tokens.
+    e = e.replace(search, getPathReplacement);
+  }
+
+  return e;
+}
+
+function getPathReplacement(match, p1) {
+  return config.paths[p1];
 }
