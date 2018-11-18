47bb04a92e8c437a2af3755e2ffd9ef9a51125bc
Rick Hawkins <rick.hawkins@slalom.com>
2018-11-17 17:29:43 -0800

    Overhaul the install script

--- scripts/npm/install.js
+++ scripts/npm/install.js
@@ -18,22 +18,34 @@ config.paths = loadConfig('paths');
 // Load libraries configuration.
 config.libraries = loadConfig('libraries');
 
-// Copy library files.
-del(config.paths.libraries_dir).then(paths => {
-  config.libraries.forEach(function (element) {
-    copy_libraries(element);
-  });
+// Copy the library files.
+Object.keys(config.libraries).forEach(function (key, index) {
+  copy_libraries(config.libraries[key]);
 });
 
 // FUNCTIONS //
 
 function loadConfig(name) {
   let ymlFile = fs.readFileSync(config_path + '/' + name + '.yml', 'utf8');
-  return yaml.load(ymlFile);
+  let config = yaml.load(ymlFile);
+
+  return config;
 }
 
 function copy_libraries(element) {
   let paths = [];
+
+  // The 'flat' option doesn't flatten the file structure, which must be a
+  // bug. Luckily, setting 'up' to true has the desired flattening effect.
+  let options = {
+    flat: true,
+    up: true
+  };
+
+  // Set any files to exclude.
+  if (typeof element.exclude != "undefined") {
+    options.exclude = element.exclude;
+  }
 
   // Ensure src is an array.
   if (typeof element.src == 'string') {
@@ -44,8 +56,9 @@ function copy_libraries(element) {
   paths.push(...element.src);
 
   // Add the destination path.
-  paths.push(config.paths.libraries_dir + '/' + element.dest);
+  element.dest = element.dest.replace("{libraries}", config.paths.libraries);
+  paths.push(element.dest);
 
-  // Copy the files, flattening all the output into one directory.
-  return copy(paths, true, function () { });
+  // Copy the files.
+  return copy(paths, options, function () { });
 }
